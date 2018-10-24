a037ba25137eab676bc7db5c24666d07ac422c2e
Joe <pumphrey.joseph@gmail.com>
2018-10-24 14:23:38 -0700

    My final commit from my project yesterday. commiting it so I can start working on lab 3

---
+++ app.js
@@ -0,0 +1,55 @@
+'use strict';
+// this app asks five yes or no questions 
+alert('Hi! Welcome to my website, lets see how well you know me!');
+// Tells the user to answer with y/yes/n/no
+alert('Answer with Y/YES/N/NO')
+
+// asks if I am from Seattle
+var questionLoc = prompt('Am I from Seattle?').toUpperCase();
+if (questionLoc === 'N' || questionLoc === 'NO') {
+    alert('You are correct! I am from Oregon.');
+} 
+else {
+    alert('Better luck next time!');
+}
+console.log('users answer to if I am from seattle:', questionLoc);
+
+// asks if I can code in javasript
+var questionCode = prompt('Can I code in JavaScript').toUpperCase();
+if (questionCode === 'Y' || questionCode === 'YES') {
+    alert('You are correct! this code was written in JavaScript.');
+} 
+else {
+    alert('Better luck next time!');
+}
+console.log('users answer to if I can code in javascript:', questionCode);
+
+// asks if I am your friend
+var questionFriend = prompt('Can I be your friend').toUpperCase();
+if (questionFriend === 'Y' || questionFriend === 'YES') {
+    alert('Awesome! I cant wait');
+}
+else {
+    alert('I will see you at PAX then!!!!');
+}
+console.log('users answer to if I am their friend:', questionFriend);
+
+// asks if I am good looking
+var questionLooks = prompt('Do I look good?').toUpperCase();
+if (questionLooks === 'Y' || questionLooks === 'YES') {
+    alert('thanks :)');
+}
+else {
+    alert('yes*');
+}
+console.log('users answer to if I amtheir friend:', questionLooks);
+
+// asks if I like the rain.
+var questionRain = prompt('Do I like the rain?').toUpperCase();
+if (questionRain === 'Y' || questionRain === 'YES') {
+    alert('I love it!!!');
+} 
+else {
+    alert('Better luck next time!');
+}
+console.log('users answer to if I like rain:', questionsRain);
